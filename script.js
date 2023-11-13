0996f3ec0aee6ce7ef1685b2b68ca5bc89155d68
HeloiseAndelhofs <heloise.andelhofs@proximus.be>
2023-11-13 15:55:25 +0100

    buttonss working with clicks and right values

--- script.js
+++ script.js
@@ -1,45 +1,55 @@
-// const timerChoices = document.querySelectorAll(".timerChoice");
-// const customTimerInput = document.getElementById("custom");
+const timerChoices = document.querySelectorAll(".timerChoice");
+const customTimerInput = document.getElementById("custom");
 const timeLeftDisplay = document.getElementById("timeLeft");
 const timeEndDisplay = document.getElementById("timeEnd");
 const stopTimer = document.getElementById("stop");
 
-// const currentTime = new Date(Date.now()).toLocaleTimeString();
-// console.log(currentTime);
+console.log(customTimerInput);
 
-const timerChoices = 22620000;
-const arrivedTime = new Date(Date.now() + timerChoices);
-let x = setInterval(function log() {
-    const customTimerInput = 390000;
+let x;
+
+
+function countdownTimer(time){
+
+    clearInterval(x)
+
+
+    const arrivedTime = new Date(Date.now() + time);
+
+
+    x = setInterval(() => {
+        
+        // const arrivedTimeCustom = new Date(Date.now() + customTimerInput);
+        
+        
+        const distance = arrivedTime - Date.now();
     
-    const arrivedTimeCustom = new Date(Date.now() + customTimerInput);
     
     
-    const distance = arrivedTime - Date.now();
-    console.log(distance);
-    timeLeftDisplay.innerHTML = distance;
-    // console.log(arrivedTime);
-}, 1000);
+    
+        const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+        timeLeftDisplay.innerHTML = hours + "h "
+        + minutes + "m " + seconds + "s ";
+    
+    }, 1000);
+};
+
+
 
 function stopCounter(){
     clearInterval(x);
-}
+    x = null;
+};
+
+timerChoices.forEach((el) => {
+    el.addEventListener("click", function(){
+        const dataTime = el.getAttribute("data-time");
+        countdownTimer(parseInt(dataTime));
+    });
+});
 
 
-// function timer(seconds) {
-//     const date = new Date();
-//     timeLeftDisplay.innerHTML = date.toLocaleTimeString()
-// };
-
-// clearInterval(myInterval);
-
-
-// timerChoice.addEventListener("click", timer);
-
-
-
-
-// timerChoices.addEventListener("click", log);
-
-
-stopTimer.addEventListener("click", stopCounter)
+stopTimer.addEventListener("click", stopCounter);

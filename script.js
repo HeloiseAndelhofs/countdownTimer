ac50fc08d93b15e98077208ea6308a1e7c369626
HeloiseAndelhofs <heloise.andelhofs@proximus.be>
2023-11-16 12:05:03 +0100

    one function with if else

--- script.js
+++ script.js
@@ -5,15 +5,16 @@ const timeLeftDisplay = document.getElementById("timeLeft");
 const timeEndDisplay = document.getElementById("timeEnd");
 const stopTimer = document.getElementById("stop");
 let x; // paramètre de clear interval
+let arrivedTime;
 
 // functions
 
 // reutilisable pour formater la date de millisecondes en hh:mm:ss
 function formatedDate(distance){
 
-    const hours = Math.round((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const minutes = Math.round((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const seconds = Math.round((distance % (1000 * 60)) / 1000);
+    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
     
     console.log(distance);
 
@@ -22,24 +23,66 @@ function formatedDate(distance){
 }
 
 // countdown timer des boutons
-function countdownTimer(time){
+// function countdownTimer(time){
 
-    clearInterval(x)
+//     clearInterval(x)
 
-    const arrivedTime = new Date(Date.now() + time);
+//     const arrivedTime = new Date(Date.now() + time);
 
-    x = setInterval(() => {
+//     x = setInterval(() => {
                 
-        const distance = arrivedTime - Date.now();
+//         const distance = arrivedTime - Date.now();
     
 
 
-        formatedDate(distance)
+//         formatedDate(distance)
     
-    }, 1000);
+//     }, 1000);
 
-    timeEndDisplay.innerHTML = `Timer will end at ${arrivedTime.getHours().toString().padStart(2, "0")}:${arrivedTime.getMinutes().toString().padStart(2, "0")}`;
-};
+//     timeEndDisplay.innerHTML = `Timer will end at ${arrivedTime.getHours().toString().padStart(2, "0")}:${arrivedTime.getMinutes().toString().padStart(2, "0")}`;
+// };
+
+
+
+
+// add event listener
+
+// add event listener et fonction de l'input
+// document.getElementById("customTimer").addEventListener("submit", (event) => {
+
+//     event.preventDefault()
+    
+//     let customTimerInput = document.getElementById("custom").value;
+    
+//     document.getElementById("customTimer").reset();
+
+//     clearInterval(x);
+
+//     const inputMilliseconds = parseInt(customTimerInput * 60000);
+
+//     const arrivedTime = new Date(Date.now() + inputMilliseconds);
+    
+//     x = setInterval(() => {
+//         const distance = arrivedTime - Date.now();
+        
+        
+//     formatedDate(distance)
+        
+//     }, 1000)
+
+//     timeEndDisplay.innerHTML = `Timer will end at ${arrivedTime.getHours().toString().padStart(2, "0")}:${arrivedTime.getMinutes().toString().padStart(2, "0")}`;
+// })
+
+
+
+
+
+
+
+
+
+
+
 
 
 // arreter le timer
@@ -47,41 +90,41 @@ function stopCounter(){
     clearInterval(x);
 };
 
+function countdownTimer(time){
+    clearInterval(x); 
 
-// add event listener
 
-// add event listener et fonction de l'input
-document.getElementById("customTimer").addEventListener("submit", (event) => {
+    x = setInterval(() => {
+        if (timerChoices) {() => {
+            
+            const arrivedTime = new Date(Date.now() + time);
 
-    event.preventDefault()
+            const distance = arrivedTime - Date.now();
+
+            formatedDate(distance)
+        }
+        } else if(document.getElementById("customTimer")) {(event) => {
+
+            event.preventDefault()
     
-    let customTimerInput = document.getElementById("custom").value;
-    
-    document.getElementById("customTimer").reset();
-
-    clearInterval(x);
-
-    const inputMilliseconds = parseInt(customTimerInput * 60000);
-
-    const arrivedTime = new Date(Date.now() + inputMilliseconds);
-    
-    // x = setInterval(() => {
-        const distance = arrivedTime - Date.now();
+            let customTimerInput = document.getElementById("custom").value;
+            
+            document.getElementById("customTimer").reset();
         
+            clearInterval(x);
         
-    formatedDate(distance)
+            const inputMilliseconds = parseInt(customTimerInput * 60000);
         
-    // }, 1000)
+            const arrivedTime = new Date(Date.now() + inputMilliseconds);
 
+            const distance = arrivedTime - Date.now();
+        
+            formatedDate(distance)
+        }
+        }
+    }, 1000)
     timeEndDisplay.innerHTML = `Timer will end at ${arrivedTime.getHours().toString().padStart(2, "0")}:${arrivedTime.getMinutes().toString().padStart(2, "0")}`;
-})
-
-
-
-
-
-
-
+} 
 // add event listener pour les boutons
 timerChoices.forEach((el) => {
     el.addEventListener("click", function(){
@@ -89,6 +132,8 @@ timerChoices.forEach((el) => {
         countdownTimer(parseInt(dataTime));
     });
 });
+
+document.getElementById("customTimer").addEventListener("submit", countdownTimer)
 
 // add event listener stop
 stopTimer.addEventListener("click", stopCounter);
